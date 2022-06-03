5a2ddd74abcdb9a2eca443605b55f13c005ec8c8
exttonymoraes <ext.tony.moraes@dafiti.com.br>
2022-06-03 00:07:40 -0300

    fix json file reading

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,57 @@ import {
   selectElement,
 } from "./util";
 
-const assignments = await getDataFromJson("../json/portfolio-items.json");
+/**
+ * to-do: check if github allows file reading
+ */
+//const assignments = await getDataFromJson("../json/portfolio-items.json");
 
+const assignments = [
+    {
+    "label": "Week01 - L01 Notes",
+    "url": "week01/l01.html"
+    },
+    {
+    "label": "Week01 - L02 Notes",
+    "url": "week01/l02.html"
+    },
+    {
+    "label": "Week02 - L03 Notes",
+    "url": "week02/l03.html"
+    },
+    {
+    "label": "Week02 - L04 Notes",
+    "url": "week02/l04.html"
+    },
+    {
+    "label": "Week03 - L05 Notes",
+    "url": "week03/l05.html"
+    },
+    {
+    "label": "Week03 - L06 Notes",
+    "url": "week03/l06.html"
+    },
+    {
+    "label": "Week04 - L07 Notes",
+    "url": "week04/l07.html"
+    },
+    {
+    "label": "Week04 - L08 Notes",
+    "url": "week04/l08.html"
+    },
+    {
+    "label": "CORS to Sleep Outside",
+    "url": "https://cors-anywhere.herokuapp.com/corsdemo"
+    },
+    {
+    "label": "Sleep Outside Project",
+    "url": "https://jmmonjeremy.github.io/L02_Team_Activity_1/build/"
+    },
+    {
+    "label": "My Livrary Project",
+    "url": "https://wmsmckay.github.io/WDD330-groupProject/build/"
+    }    
+];
 function preparePortfolioItem(templateClone, data) {
   const item = templateClone.querySelector("a");
   item.href = data.url;
